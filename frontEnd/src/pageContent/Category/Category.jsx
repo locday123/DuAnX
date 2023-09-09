a890efc9637e6d33dbecd4a0e855de9c9c9bffc2
Xuan-Loc <xuanloc060393@gmail.com>
2023-09-09 16:28:40 +0700

    09/09/2023

--- frontEnd/src/pageContent/Category/Category.jsx
+++ frontEnd/src/pageContent/Category/Category.jsx
@@ -1,18 +1,44 @@
 import { useEffect, useState } from "react";
+import { TreeSelect } from 'antd';
+
 import { getCategory } from '../../Service/Category/CategoryService'
-import TreeSelect from 'mui-tree-select'
 function Category() {
+  const { SHOW_PARENT } = TreeSelect;
   const [category, setCategory] = useState([])
+  const [value, setValue] = useState([]);
 
   useEffect(() => {
     getCategory().then((value) => {
-      setCategory(value.category.at(0))
+      setCategory(value.category)
     });
   }, [])
-  console.log(category);
+  const onChange = (newValue) => {
+    console.log('onChange', value);
+    setValue(newValue);
+  };
+
+
   return (
     <div>
-      abc
+      <TreeSelect
+        fieldNames={{
+          children: 'childCategory',
+          label: 'nameCategory',
+          value: 'idCategory',
+          link: 'linkCategory'
+        }}
+        showSearch
+        style={{ width: '20%' }}
+        value={value}
+        dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+        placeholder="Please select"
+        allowClear
+        multiple
+
+        onChange={onChange}
+        treeData={category}
+
+      />
     </div>
   )
 }
