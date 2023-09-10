366f46f6387954cf7b351854586e1f4c92762f3d
Xuan-Loc <xuanloc060393@gmail.com>
2023-09-10 18:41:11 +0700

    10/09/2023 - 3

--- frontEnd/src/pageContent/Category/CategoryList.jsx
+++ frontEnd/src/pageContent/Category/CategoryList.jsx
@@ -1,9 +1,12 @@
 import { Box } from "@mui/material"
 
 function CategoryList({ value }) {
-    return (
-        <Box>
 
+    return (
+        <Box sx={{ display: "flex" }}>
+            <Box>{value.nameCategory}</Box>
+            <Box>{value.idCategory}</Box>
+            <Box>{value.linkCategory}</Box>
         </Box>
     )
 }
