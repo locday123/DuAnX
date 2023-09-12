fd8571eb85234ed767080678c49f8bb39debf7c6
Xuan-Loc <xuanloc060393@gmail.com>
2023-09-12 22:20:22 +0700

    Toi 12/09/2023

--- Backend/connect/SQL/trigger_sql.sql
+++ Backend/connect/SQL/trigger_sql.sql
@@ -7,12 +7,13 @@ SET NEW.idAccount = CONCAT('S',LPAD(
   FROM INFORMATION_SCHEMA.TABLES    
   WHERE table_name = 'ACCOUNT')
 ,3,'0'));
-delimiter 
-CREATE TRIGGER rootCategoryEdit AFTER INSERT ON CATEGORY
+
+delimiter //
+CREATE TRIGGER rootCategoryEdit BEFORE INSERT ON CATEGORY
 FOR EACH ROW 
 BEGIN
-	IF (NEW.rootCategory IS NULL) THEN
-		SET NEW.rootCategory = 0
-	END IF
-END
+	IF NEW.rootCategory IS NULL THEN
+		SET NEW.rootCategory = 0;
+	END IF;
+END; //
 delimiter ;
