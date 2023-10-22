c2ba601c65a0c52f2ac3cb24d68f941d397f016d
Xuan-Loc <xuanloc060393@gmail.com>
2023-10-22 16:18:05 +0700

    22/10/2023

--- frontEnd/src/pageContent/Product/AddProduct.jsx
+++ frontEnd/src/pageContent/Product/AddProduct.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, FormControlLabel, List, ListItem, Switch, TextField } from "@mui/material"
+import { Box, Button, FormControlLabel, List, ListItem, MenuItem, Switch, TextField } from "@mui/material"
 
 function AddProduct() {
     const inputCloumn_1 = [
@@ -7,67 +7,93 @@ function AddProduct() {
         { nameInput: 'metaDescription', placehoder: 'Vui lòng nhập Meta Description', labelInput: 'Meta Description', typeInput: 'text' },
         { nameInput: 'metaKeyword', placehoder: 'Vui lòng nhập từ khóa', labelInput: 'Meta Keyword', typeInput: 'text' },
     ]
-
+    const sexAccount = [
+        { valueInput: 2, label: 'Vui lòng chọn giới tính' },
+        { valueInput: 0, label: 'Nam' },
+        { valueInput: 1, label: 'Nữ' }
+      ]
     const inputCloumn_2 = [
         { nameInput: 'nameProduct', placehoder: 'Vui lòng nhập tên Sản phẩm', labelInput: 'Tên Sản phẩm', typeInput: 'text' },
-        { nameInput: 'priceProduct', placehoder: 'Vui lòng nhập Giá sản phẩm', labelInput: 'Giá Sản phẩm', typeInput: 'text' },
-        { nameInput: 'priceThrough', placehoder: 'Vui lòng nhập Giá thị trường', labelInput: 'Giá thị trường', typeInput: 'text' },
+        { nameInput: 'idStorage', placehoder: 'Vui lòng chọn dung lượng', labelInput: 'Dung lượng', typeInput: 'select' },
+        { nameInput: 'idCategory', placehoder: 'Vui lòng chọn danh mục', labelInput: 'Danh mục', typeInput: 'select' },
+        { nameInput: 'priceProduct', placehoder: 'Vui lòng nhập Giá sản phẩm', labelInput: 'Giá Sản phẩm', typeInput: 'number' },
+        { nameInput: 'priceThrough', placehoder: 'Vui lòng nhập Giá thị trường', labelInput: 'Giá thị trường', typeInput: 'number' },
     ]
     return (
         <Box sx={{ display: "flex" }}>
-            <Box sx={{ width: "60%", borderRadius: "10px", marginRight: "10px" }}>
+            <Box sx={{ width: "70%", borderRadius: "10px", marginRight: "10px" }}>
                 <Box sx={{ backgroundColor: "white", borderRadius: "10px" }}>
                     <List>
                         <ListItem>
                             <Box sx={{ display: "flex" }}>
-                            <img style={{ marginRight: "30px", borderRadius: "8px" }} height="120" width="120px" src="http://localhost:8081/images/img_avatar.png" />
-                            <Box>
-                                <Button variant="contained" component="label" sx={{ width: "200px", marginRight: "10px" }}>
-                                    <TextField
-                                        label={""}
-                                        placeholder={"value.placehoder"}
-                                        name={"value.nameInput"}
-                                        type="file"
-                                        fullWidth
-                                        hidden
-                                    />
-                                    {"value.labelInput"}
-                                </Button> {/* Thêm hình ảnh */}
-                            </Box>
+                                <img style={{ marginRight: "30px", borderRadius: "8px" }} height="120" width="120px" src="http://localhost:8081/images/img_avatar.png" />
+                                <Box>
+                                    <Button variant="contained" component="label" sx={{ width: "250px", marginRight: "10px" }}>
+                                        <TextField
+                                            name="imageProduct"
+                                            type="file"
+                                            fullWidth
+                                            hidden
+                                        />
+                                        {"THÊM HÌNH ẢNH SẢN PHẨM"}
+                                    </Button> {/* Thêm hình ảnh */}
+                                </Box>
                             </Box>
                         </ListItem>
                     
                     {
                         inputCloumn_2.map((value, index) => (
-                        <ListItem>
-                            <TextField
-                                label={value.labelInput}
-                                placeholder={value.placehoder}
-                                name={value.nameInput}
-                                size="small"
-                                fullWidth
-                                    />
-                        </ListItem>
+                            value.typeInput == "select" ?
+                            <ListItem key={index}>
+                                <TextField
+                                    label={value.labelInput}
+                                    placeholder={value.placehoder}
+                                    type={value.typeInput}
+                                    name={value.nameInput}
+                                    size="medium"
+                                    select
+                                    fullWidth
+                                >
+                                    {
+                                        sexAccount.map((value) => (
+                                            <MenuItem key={value.valueInput} value={value.valueInput}>
+                                                {value.label}
+                                            </MenuItem>
+                                        ))
+                                    }
+                                </TextField>
+                            </ListItem>
+                            :
+                            <ListItem>
+                                <TextField
+                                    label={value.labelInput}
+                                    placeholder={value.placehoder}
+                                    type={value.typeInput}
+                                    name={value.nameInput}
+                                    size="medium"
+                                    fullWidth
+                                />
+                            </ListItem>
                         ))
                         
                     }
                     </List>
                 </Box>
             </Box>
-            <Box sx={{ width: "40%"}}>
+            <Box sx={{ width: "30%"}}>
                 <Box sx={{backgroundColor: "white", borderRadius: "10px" }}>
                     <List>
                     {
                         inputCloumn_1.map((value, index) => (
-                        <ListItem>
-                            <TextField
-                                label={value.labelInput}
-                                placeholder={value.placehoder}
-                                name={value.nameInput}
-                                size="medium"
-                                fullWidth
-                                    />
-                        </ListItem>
+                            <ListItem key={index}>
+                                <TextField
+                                    label={value.labelInput}
+                                    placeholder={value.placehoder}
+                                    name={value.nameInput}
+                                    size="medium"
+                                    fullWidth
+                                />
+                            </ListItem>
                         ))
                         
                     }
