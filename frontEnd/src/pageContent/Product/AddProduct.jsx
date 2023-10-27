7ae16f888abbc58d070be8f97dae91aa890130b0
Xuan-Loc <xuanloc060393@gmail.com>
2023-10-27 20:04:09 +0700

    27/10/2023

--- frontEnd/src/pageContent/Product/AddProduct.jsx
+++ frontEnd/src/pageContent/Product/AddProduct.jsx
@@ -1,3 +1,5 @@
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic"
+import { CKEditor } from "@ckeditor/ckeditor5-react"
 import { Box, Button, FormControlLabel, List, ListItem, MenuItem, Switch, TextField } from "@mui/material"
 
 function AddProduct() {
@@ -19,10 +21,15 @@ function AddProduct() {
         { nameInput: 'priceProduct', placehoder: 'Vui lòng nhập Giá sản phẩm', labelInput: 'Giá Sản phẩm', typeInput: 'number' },
         { nameInput: 'priceThrough', placehoder: 'Vui lòng nhập Giá thị trường', labelInput: 'Giá thị trường', typeInput: 'number' },
     ]
+    const inputCKEEditor = [
+        { nameInput: 'productBox', placehoder: 'Thông tin của máy Box / Unbox', labelInput: 'Sản phẩm có gì ?', typeInput: 'text' },
+        { nameInput: 'shortDescription', placehoder: 'Vui lòng nhập Mô rả ngắn', labelInput: 'Mô tả ngắn', typeInput: 'text' },
+        { nameInput: 'reviewArticle', placehoder: 'Vui lòng soạn bài viết đánh giá', labelInput: 'Bài viết đánh giá', typeInput: 'text' },
+    ]
     return (
         <Box sx={{ display: "flex" }}>
             <Box sx={{ width: "60%", marginRight: "10px" }}>
-                <Box sx={{ backgroundColor: "white", borderRadius: "10px" }}>
+                <Box sx={{ backgroundColor: "white", borderRadius: "10px", width:"100%" }}>
                     <List>
                         <ListItem>
                             <Box sx={{ display: "flex" }}>
@@ -42,41 +49,59 @@ function AddProduct() {
                         </ListItem>
                     
                     {
-                        inputCloumn_2.map((value, index) => (
-                            value.typeInput == "select" ?
-                            <ListItem key={index}>
-                                <TextField
-                                    label={value.labelInput}
-                                    placeholder={value.placehoder}
-                                    type={value.typeInput}
-                                    name={value.nameInput}
-                                    size="medium"
-                                    select
-                                    fullWidth
-                                >
-                                    {
-                                        sexAccount.map((value) => (
-                                            <MenuItem key={value.valueInput} value={value.valueInput}>
-                                                {value.label}
-                                            </MenuItem>
-                                        ))
-                                    }
-                                </TextField>
-                            </ListItem>
-                            :
-                            <ListItem>
-                                <TextField
-                                    label={value.labelInput}
-                                    placeholder={value.placehoder}
-                                    type={value.typeInput}
-                                    name={value.nameInput}
-                                    size="medium"
-                                    fullWidth
-                                />
-                            </ListItem>
-                        ))
-                        
-                    }
+                    inputCloumn_2.map((value, index) => (
+                        value.typeInput == "select" ?
+                        <ListItem key={index}>
+                            <TextField
+                                label={value.labelInput}
+                                placeholder={value.placehoder}
+                                type={value.typeInput}
+                                name={value.nameInput}
+                                size="medium"
+                                select
+                                fullWidth
+                            >
+                                {
+                                    sexAccount.map((value) => (
+                                        <MenuItem key={value.valueInput} value={value.valueInput}>
+                                            {value.label}
+                                        </MenuItem>
+                                    ))
+                                }
+                            </TextField>
+                        </ListItem>
+                        :
+                        <ListItem>
+                            <TextField
+                                label={value.labelInput}
+                                placeholder={value.placehoder}
+                                type={value.typeInput}
+                                name={value.nameInput}
+                                size="medium"
+                                fullWidth
+                            />
+                        </ListItem>
+                    ))}
+                    {
+                    inputCKEEditor.map((value, index) => (
+                        <ListItem key={index} >
+                            <div style={{width:"100%"}}>
+                            <CKEditor
+                                editor={ClassicEditor}
+                                onReady={(editor)=>{
+                                    editor.editing.view.change((writer) => {
+                                    writer.setStyle(
+                                       //use max-height(for scroll) or min-height(static)
+                                       "height", 
+                                       "200px",
+                                       editor.editing.view.document.getRoot()
+                                    );
+                                 });
+                                 }}
+                            />
+                            </div>
+                        </ListItem>
+                    ))}
                     </List>
                 </Box>
             </Box>
@@ -84,26 +109,23 @@ function AddProduct() {
                 <Box sx={{backgroundColor: "white", borderRadius: "10px" }}>
                     <List>
                     {
-                        inputCloumn_1.map((value, index) => (
-                            <ListItem key={index}>
-                                <TextField
-                                    label={value.labelInput}
-                                    placeholder={value.placehoder}
-                                    name={value.nameInput}
-                                    size="medium"
-                                    fullWidth
-                                />
-                            </ListItem>
-                        ))
-                        
-                    }
+                    inputCloumn_1.map((value, index) => (
+                        <ListItem key={index}>
+                            <TextField
+                                label={value.labelInput}
+                                placeholder={value.placehoder}
+                                name={value.nameInput}
+                                size="medium"
+                                fullWidth
+                            />
+                        </ListItem>
+                    ))}
                         <ListItem>
                             <Box>
                                 <FormControlLabel
                                     value="start"
                                     control={<Switch color="primary" />}
                                     label="Ẩn / Hiện Sản phẩm"
-                                
                                     labelPlacement="start"
                                 />
                             </Box>
