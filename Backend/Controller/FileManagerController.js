f7844a8da66dcb84a7bead675a2e32f03afe81a7
Xuan-Loc <xuanloc060393@gmail.com>
2023-11-16 01:43:45 +0700

    16/11/2023

--- Backend/Controller/FileManagerController.js
+++ Backend/Controller/FileManagerController.js
@@ -1,16 +1,18 @@
 const fs = require('fs')
 
-function listDirectories(pth) {
-    const directories = (fs.readdirSync(pth, {withFileTypes: true}))
+function listDirectories(path, parent) {
+    const data = []
+    const directories = (fs.readdirSync(path, {withFileTypes: true}))
       .filter(dirent => dirent.isDirectory())
-      .map(dir => dir.name);
-  
-    return directories;
+        .map(dir => data.push(
+            { ["title"]: dir.name, ["nameFolder"]: dir.name, ["parentFolder"]: parent, ["path"]: dir.name+"/", ["key"]:dir.name }
+        ));
+    return data;
 }
   
 module.exports = {
     getAll: (req, res) => {
         var data = req.body.path ? req.body.path : ""
-        return res.json( listDirectories("public/"+data)) 
+        return res.json( listDirectories("public/"+data, data)) 
     }
 }
