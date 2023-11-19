811249ebc534179ebf9bad3d0f2d07d2c190ba51
Xuan-Loc <xuanloc060393@gmail.com>
2023-11-19 16:09:46 +0700

    19/11/2023

--- Backend/Controller/FileManagerController.js
+++ Backend/Controller/FileManagerController.js
@@ -1,4 +1,7 @@
 const dree = require('dree')
+const path = require('path')
+const fs = require('fs');
+const { log } = require('console');
 const options = {
     stat: false,
     hash: false,
@@ -6,10 +9,38 @@ const options = {
     size: true,
     normalize: true,
 };
-const treeFolder = dree.scan('public', options)
+
+function addNewFolder(filePath) {
+    var dirname = path.resolve(path.join(filePath));
+    if (fs.existsSync(dirname)) {
+      return true;
+    }
+    fs.mkdirSync(filePath, { recursive: true });
+}
+
+  
 module.exports = {
     getAll: (req, res) => {
-        var data = req.body.path ? req.body.path : ""
-        return res.json([treeFolder]) 
+        var pathFolder = req.body.pathFolder
+        var action = req.body.action
+        var nameFolder = req.body.nameFolder
+        
+        if (action == "read") {
+            return res.json([dree.scan(pathFolder, options)]) 
+        }
+        if (action == "create-folder")
+        {
+            if (addNewFolder('.//' + pathFolder + "//" + nameFolder))
+            {
+                res.json({status: "Thư mục đã tồn tại"})
+            }
+            else {
+                res.json({status: "Tạo thư mục thành công"})
+            }
+        }
+        if (action == "delete-folder") {
+            fs.rm()
+        }
+        
     }
 }
